73cbb40f9da00eea781ca55bb99f184161fb0d5c
Southchess <Admin@Thomas-MacBook-Air.local>
2015-06-13 10:16:40 -0600

    Created camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	
+	private Vector3 offset;
+	
 	// Use this for initialization
 	void Start () {
-	
+		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () { //late means runs after all processes updated
+		//each frame camera is moved along with player
+		transform.position = player.transform.position + offset;
 	}
 }
