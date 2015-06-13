39b012eb3f917d131f0fff5f00e5f76be33f4cfb
Southchess <Admin@Thomas-MacBook-Air.local>
2015-06-13 12:16:27 -0600

    Messing with roof

---
+++ Assets/Scripts/Blast.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blast : MonoBehaviour {
+	public Rigidbody energBlast;
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightShift)) {
+			//GameObject.CreatePrimitive(PrimitiveType.Cube);	
+			//Rigidbody clone;
+
+			Rigidbody instantiatedProjectile = Instantiate(energyBlast,transform.position,transform.rotation)as Rigidbody;
+			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0,10));
+
+			//for (int i = 0; i < 1; i++) {
+			//	Instantiate(Cube, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+			//}
+
+
+
+
+
+
+
+
+		}
+	}
+}
