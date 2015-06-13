32fc95228fd196ec03535133617a1e7e9406cac7
SwordShieldMouse <alke8882000@gmail.com>
2015-06-13 09:47:51 -0600

    Added basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float moveSpeed;
+	public float flightSpeed;
+	public float jumpSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movement
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		Vector3 moveVector = new Vector3 (horizontal, vertical, 0) * moveSpeed;
+		rb.AddForce (moveVector);
+
+		// Jumping
+
+	}
+
+	void FixedUpdate() {
+
+	}
+}
